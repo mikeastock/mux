ade42dc057e49b8fcb01b57292d6dd6216902af4
Michael Stock <mikeastock@gmail.com>
2016-01-17 17:00:40 -0800

    Pull session name from arguments

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,23 @@ extern crate yaml_rust;
 mod error;
 
 use clap::{App, Arg};
-use yaml_rust::{YamlLoader, Yaml};
+use error::Result;
 use std::fs::File;
-use std::io;
 use std::io::prelude::*;
-use error::{Error, Result};
+use std::process::Command;
+use yaml_rust::{YamlLoader, Yaml};
 
 fn main() {
-    let config = match load_config() {
+    let matches = App::new("mux")
+        .arg(Arg::with_name("name")
+                .help("Name of session")
+                .required(true)
+        )
+        .get_matches();
+
+    let session_name = Some(matches.value_of("name")).unwrap().unwrap();
+
+    let config = match load_config(&session_name) {
         Ok(config) => config,
         Err(err) => {
             println!("{:?}", err);
@@ -20,18 +29,27 @@ fn main() {
         }
     };
 
+    let output = Command::new("tmux").output().unwrap();
+    println!("{:?}", output.status);
+    println!("{:?}", String::from_utf8(output.stderr));
+    println!("{:?}", output.stdout);
+    println!("{:?}", session_name);
     println!("{:?}", config["name"].as_str().unwrap());
 }
 
-fn load_config() -> Result<Yaml> {
-    let file_string = try!(load_yaml());
+fn load_config(session_name: &str) -> Result<Yaml> {
+    let file_string = try!(load_yaml(session_name));
     let yamls = try!(YamlLoader::load_from_str(&file_string));
     Ok(yamls[0].to_owned())
 }
 
-fn load_yaml() -> Result<String> {
-    let mut file = try!(File::open("/Users/mikeastock/.tmuxinator/dev.yml"));
+fn load_yaml(session_name: &str) -> Result<String> {
+    let mut file = try!(File::open(session_filepath(session_name)));
     let mut file_string = String::new();
     try!(file.read_to_string(&mut file_string));
     Ok(file_string)
+}
+
+fn session_filepath(name: &str) -> String {
+    "/Users/mikeastock/.tmuxinator/".to_owned() + name + ".yml"
 }
