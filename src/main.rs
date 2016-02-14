aaebbcc8e3a79e534630bee59cbf0f203cf2a66d
Michael Stock <mikeastock@gmail.com>
2016-02-13 17:52:00 -0800

    Launch tmux session into child process

--- src/main.rs
+++ src/main.rs
@@ -29,12 +29,15 @@ fn main() {
         }
     };
 
-    let output = Command::new("tmux").output().unwrap();
+    println!("{:?}", session_name);
+    println!("{:?}", config["name"].as_str().unwrap());
+
+    let child = Command::new("tmux").spawn().unwrap();
+    let output = child.wait_with_output().unwrap();
+
     println!("{:?}", output.status);
     println!("{:?}", String::from_utf8(output.stderr));
     println!("{:?}", output.stdout);
-    println!("{:?}", session_name);
-    println!("{:?}", config["name"].as_str().unwrap());
 }
 
 fn load_config(session_name: &str) -> Result<Yaml> {
